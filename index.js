943185cd364a87a846bf4f418518a6121445dfd4
Randy Truong <truongrandy@gmail.com>
2019-07-09 07:58:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // write your code below!
+function happyHolidays(){
+  console.log("Happy Holidays!")
+}
+
+function happyHolidaysTo(string){
+  console.log(`Happy holidays, ${name}!`)
+}
+
+function holidayCountdown(string,string){
+  console.log(`It's ${days} days until ${holiday}!`)
+}
