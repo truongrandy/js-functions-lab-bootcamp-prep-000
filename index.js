e3e967a1d452328e9f8eea585ed74c692a639d1f
Randy Truong <truongrandy@gmail.com>
2019-07-09 07:59:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // write your code below!
 function happyHolidays(){
-  console.log("Happy Holidays!")
+  return("Happy Holidays!")
 }
 
 function happyHolidaysTo(string){
-  console.log(`Happy holidays, ${name}!`)
+  return(`Happy holidays, ${name}!`)
 }
 
 function holidayCountdown(string,string){
-  console.log(`It's ${days} days until ${holiday}!`)
+  return(`It's ${days} days until ${holiday}!`)
 }
